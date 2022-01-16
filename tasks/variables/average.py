aea94c583b793f82e80e6b1cd40367cdd676d52c
vlad ratnikov <iamratnikov@yandex.by>
2022-01-16 22:32:48 +0300

    this done2

--- tasks/variables/average.py
+++ tasks/variables/average.py
@@ -5,6 +5,7 @@
 которая возвращает среднее арифметическое аргументов
 и округляет его до 5 знаков после запятой.
 """
+import math
 from typing import Union
 
 
@@ -18,7 +19,7 @@ def calc_average(a: int, b: int, c: int) -> Union[int, float]:
 
     :return: среднее арифметическое, округленное до 5 знаков
     """
-    result = None
+    result = round((a + b + c) / 3, 5)
     return result
 
 
